0e1c4e714a6c4d431fb064df4a8123c557e89aeb
Sofiane <sofianefugon@icloud.com>
2026-05-06 15:01:43 +0200

    partie2

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,63 @@
 import factory.TripFactory;
 import models.Trip;
 import exo.Partie1;
+import exo.Partie2;
+import exo.Partie3;
+import exo.Partie4;
 
 void main() {
 
     // appeler les méthodes des exos ici
     // pour tester si ça marche bien, générer une liste de 10 éléments et afficher le résultat
 
-    System.out.println("Partie 1");
+    System.out.println("Partie 1\n");
 
     Partie1 partie1 = new Partie1();
 
-    System.out.println("exo1");
+    System.out.println("exo1\n");
     List<Trip> trips = TripFactory.generateTrips(10);
 
     List<Trip> result = partie1.longAndExpensiveTrips(trips);
     result.forEach(System.out::println);
 
-    System.out.println("exo2");
+    System.out.println("exo2\n");
     trips = TripFactory.generateTrips(10);
 
     result = partie1.badTrips(trips);
     result.forEach(System.out::println);
 
-    System.out.println("exo3");
+    System.out.println("exo3\n");
     trips = TripFactory.generateTrips(20);
 
     result = partie1.recentTrips(trips);
     result.forEach(System.out::println);
+
+    System.out.println("Partie 2\n");
+
+    Partie2 partie2 = new Partie2();
+
+    System.out.println("exo4 \n");
+    trips = TripFactory.generateTrips(10);
+
+    Map<String, Long> result4 = partie2.countByCity(trips);
+    result4.forEach((city, count) -> System.out.println(city + " " + count));
+
+    System.out.println("exo5 \n");
+    trips = TripFactory.generateTrips(10);
+
+    Map<String, Double> result5 = partie2.revenueByDriver(trips);
+    result5.forEach((driver, revenue) -> System.out.println(driver + " " + revenue));
+
+    System.out.println("exo6 \n");
+    trips = TripFactory.generateTrips(10);
+
+    Map<String, Double> result6 = partie2.avgDurationByCity(trips);
+    result6.forEach((city, count) -> System.out.println(city + " " + count));
+
+    System.out.println("Partie 3\n");
+
+
+
 
 
 }
--- src/exo/Partie2.java
+++ src/exo/Partie2.java
@@ -4,23 +4,41 @@ import models.Trip;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.ToDoubleFunction;
+import java.util.stream.Collectors;
 
 public class Partie2 {
 
+
+    private static final Function<Trip, String> groupByCity =
+            trip -> trip.city();
+
+    private static final Function<Trip, String> groupByDriver =
+            trip -> trip.driverId();
+
+    private static final ToDoubleFunction<Trip> getRevenue =
+            trip ->  trip.price();
+
+    private static final ToDoubleFunction<Trip> getDuree =
+            trip ->  trip.durationMin();
+
+
+
     public Map<String, Long> countByCity(List<Trip> trips) {
-        // coder ici
-        return Map.of();
+        return trips.stream()
+                .collect(Collectors.groupingBy(groupByCity, Collectors.counting()));
+
     }
 
     public Map<String, Double> revenueByDriver(List<Trip> trips) {
-        // revenu par chauffeur
-        // coder ici
-        return Map.of();
+        return trips.stream()
+                .collect(Collectors.groupingBy(groupByDriver, Collectors.summingDouble(getRevenue)));
     }
 
     public Map<String, Double> avgDurationByCity(List<Trip> trips) {
-        // coder ici
-        // durée moyenne par ville
-        return Map.of();
+        return trips.stream()
+                .collect(Collectors.groupingBy(groupByCity, Collectors.averagingDouble(getDuree)));
     }
 }
