45e05fc11a4ce367b3b01e531e2df807eaf20abf
Sofiane <sofianefugon@icloud.com>
2026-05-06 14:37:21 +0200

    partie1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
 import factory.TripFactory;
 import models.Trip;
+import exo.Partie1;
 
 void main() {
-    List<Trip> trips = TripFactory.generateTrips(10000000);
 
     // appeler les méthodes des exos ici
     // pour tester si ça marche bien, générer une liste de 10 éléments et afficher le résultat
+
+    System.out.println("Partie 1");
+
+    Partie1 partie1 = new Partie1();
+
+    System.out.println("exo1");
+    List<Trip> trips = TripFactory.generateTrips(10);
+
+    List<Trip> result = partie1.longAndExpensiveTrips(trips);
+    result.forEach(System.out::println);
+
+    System.out.println("exo2");
+    trips = TripFactory.generateTrips(10);
+
+    result = partie1.badTrips(trips);
+    result.forEach(System.out::println);
+
+    System.out.println("exo3");
+    trips = TripFactory.generateTrips(20);
+
+    result = partie1.recentTrips(trips);
+    result.forEach(System.out::println);
+
+
 }
--- src/exo/Partie1.java
+++ src/exo/Partie1.java
@@ -2,22 +2,44 @@ package exo;
 
 import models.Trip;
 
+import java.time.LocalDate;
+import java.util.function.Predicate;
+
 import java.util.List;
 
 public class Partie1 {
 
+    private static final Predicate<Trip> isLong =
+            trip -> trip.distanceKm() > 10;
+    private static final Predicate<Trip> isExpensive =
+            trip -> trip.price() > 20;
+    private static final Predicate<Trip> isBad =
+            trip -> trip.rating() < 3;
+    private static final Predicate<Trip> isRecent = trip -> {
+        LocalDate ajd = LocalDate.now();
+        LocalDate hier = ajd.minusDays(1);
+        LocalDate date = trip.startTime().toLocalDate();
+        return date.equals(ajd) || date.equals(hier);
+    };
+
     public List<Trip> longAndExpensiveTrips(List<Trip> trips) {
         // distance > 10km et prix > 20€
-        return List.of();
+        return trips.stream()
+                .filter(isLong.and(isExpensive))
+                .toList();
     }
 
     public List<Trip> badTrips(List<Trip> trips) {
         // rating < 3
-        return List.of();
+        return trips.stream()
+                .filter(isBad)
+                .toList();
     }
 
     public List<Trip> recentTrips(List<Trip> trips) {
         // aujourd’hui ou hier
-        return List.of();
+        return trips.stream()
+                .filter(isRecent)
+                .toList();
     }
 }
